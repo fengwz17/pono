43e732505da57d47fde1f6343065ec7f8b029f08
fengwz17 <fengwz17@outlook.com>
2022-02-25 01:50:19 +0800

    record clock in each step

--- engines/bmc.cpp
+++ engines/bmc.cpp
@@ -17,6 +17,11 @@
 #include "bmc.h"
 #include "utils/logger.h"
 
+#include <time.h>
+
+// fwz:recording the time that the solver takes in each step
+clock_t cc1, cc2, cc3, cc4;
+
 using namespace smt;
 
 namespace pono {
@@ -44,12 +49,15 @@ void Bmc::initialize()
   // future we can use solver_->reset_assertions(), but it is not currently
   // supported in boolector
   solver_->assert_formula(unroller_.at_time(ts_.init(), 0));
+  // std::cout << "init " << " ts_.init(): " << ts_.init() << std::endl;
 }
 
 ProverResult Bmc::check_until(int k)
 {
+  // cc1 = clock();
   initialize();
-
+ //  cc2 = clock();
+  // std::cout << " initialize(): " << ((double) cc2 - cc1)/CLOCKS_PER_SEC << std::endl;
   for (int i = reached_k_ + 1; i <= k; ++i) {
     if (!step(i)) {
       compute_witness();
@@ -67,13 +75,20 @@ bool Bmc::step(int i)
 
   bool res = true;
   if (i > 0) {
+    // std::cout << "trans " << i << " ts_.trans(): " << ts_.trans() << std::endl;
     solver_->assert_formula(unroller_.at_time(ts_.trans(), i - 1));
   }
 
   solver_->push();
   logger.log(1, "Checking bmc at bound: {}", i);
+  // std::cout << "checking " << i << " bad_: " << bad_ << std::endl;
   solver_->assert_formula(unroller_.at_time(bad_, i));
+  // std::cout << " i: " << i << " solver_->assert_formula(bad_): " << ((double) cc4 - cc3)/CLOCKS_PER_SEC << std::endl;
+  cc3 = clock();
   Result r = solver_->check_sat();
+  cc4 = clock();
+  std::cout << " i: " << i << " solver_->check_sat(): " << ((double) cc4 - cc3)/CLOCKS_PER_SEC << std::endl;
+  
   if (r.is_sat()) {
     res = false;
   } else {
